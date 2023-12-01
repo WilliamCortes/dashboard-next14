b5846bde69f4bf112a33e6ec2dba8e098aa68d77
WilliamCortesT <wcortes@talenta.cloud>
2023-12-01 10:20:45 -0500

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const CustomersPage = (props: Props) => {
+  return <div>customersPage</div>;
+};
+
+export default CustomersPage;
---
+++ app/hello/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Hello = (props: Props) => {
+  return <div>Hello Will, how is it is going?</div>;
+};
+
+export default Hello;
