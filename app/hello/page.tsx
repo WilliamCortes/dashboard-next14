59e3ebad2a02aad02a4dbfe14daa3fdc683c55e2
William Cortes <wcortes@magnetoglobal.com>
2024-08-07 10:55:55 -0500

    it is the time

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Hello = (props: Props) => {
-  return <div>Hello Will, how is it is going?</div>;
+  return (
+    <div>
+      Hello Will, how is it is going? <b>It is the time</b>
+    </div>
+  );
 };
 
 export default Hello;
